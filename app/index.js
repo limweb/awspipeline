b41cae7e1af7ed6596228a9ff6547b5328349b24
lim web <limweb@hotmail.com>
2020-10-14 13:51:42 +0700

    body-parser

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 const router = require('./route.js');
 
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
